1e4677ff68f13be35b0f9edba368b554ffd62ffe
lucasgiumarra <lugiumarra@gmail.com>
2025-05-22 19:40:13 -0700

    negative character groups

--- app/main.py
+++ app/main.py
@@ -17,6 +17,18 @@ def match_pattern(input_line, pattern):
             if "A" <= char <= "Z" or "a" <= char <= "z" or char == "_":
                 return True
         return False
+    # this negative character group logic must come before the positive group
+    # logic becuase otherwise a negative character group will run under a
+    # positive group logic
+    elif "[" in pattern and "]" in pattern and "^" in pattern:
+        for p in pattern:
+            if p == "[" or p == "]" or p == "^":
+                continue
+            for char in input_line:
+                if p != char:
+                    return False
+        return True
+    # positive character groups
     elif "[" in pattern and "]" in pattern:
         for p in pattern:
             if p == "[":
