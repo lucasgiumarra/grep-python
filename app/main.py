5ad74f1e6cdaa8b1203c98bd30fae4501de64ce1
lucasgiumarra <lugiumarra@gmail.com>
2025-05-28 18:24:17 -0700

    Combining character classes

--- app/main.py
+++ app/main.py
@@ -3,6 +3,62 @@ import sys
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
 
+def match(pattern, input_line):
+    for i in range(len(input_line) + 1):
+        if matchhere(pattern, input_line[i:]):
+            return True
+    return False
+
+
+def matchhere(pattern, input_line):
+    if not pattern:
+        return True
+    if input_line and pattern_matches(pattern[0], input_line[0]):
+        return True
+    return False
+
+# def matchstar():
+
+def pattern_matches(pattern, input_char):
+    if pattern == "\d":
+        for char in input_char:
+            if "0" <= char <= "9":
+                return True
+        return False
+    elif pattern == "\w":
+        for char in input_char:
+            if "A" <= char <= "Z" or "a" <= char <= "z" or char == "_":
+                return True
+        return False
+    # this negative character group logic must come before the positive group
+    # logic becuase otherwise a negative character group will run under a
+    # positive group logic
+    elif "[" in pattern and "]" in pattern and "^" in pattern:
+        for p in pattern:
+            if p == "[" or p == "]" or p == "^":
+                continue
+            if p not in input_char:
+                return True
+        return False
+    # positive character groups
+    elif "[" in pattern and "]" in pattern:
+        if "^" in pattern:
+            for p in pattern:
+                if p == "[" or p == "]" or p == "^":
+                    continue
+                if p not in input_char:
+                    return True
+            return False
+        else:
+            for p in pattern:
+                if p == "[" or p == "]":
+                    continue
+                if p in input_char:
+                    return True
+            return False
+    return False
+
+
 
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
@@ -60,6 +116,8 @@ def main():
 
     # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
+    # if match(pattern, input_line):
+        print("pattern: " + pattern, file=sys.stderr)
         print("exit 0", file=sys.stderr)
         exit(0)
     else:
