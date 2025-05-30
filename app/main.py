93cb933d2fc940ca292fd9251291c1d12ba50dd4
lucasgiumarra <lugiumarra@gmail.com>
2025-05-29 19:07:39 -0700

    end of string anchor

--- app/main.py
+++ app/main.py
@@ -33,10 +33,10 @@ def matchhere(pattern, input_line):
         if input_line and input_line[0].isalpha():
             return matchhere(pattern[2:], input_line[1:])
         return False
-    if pattern[0] == input_line[0]:
-        return matchhere(pattern[1:], input_line[1:])
     if pattern[0] == "$" and input_line == "":
         return True
+    if pattern[0] == input_line[0]:
+        return matchhere(pattern[1:], input_line[1:])
     return False
 
 # def matchstar():
