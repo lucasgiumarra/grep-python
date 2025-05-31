101361fddc17d03c6dc34e6703768c8da5e41af9
lucasgiumarra <lugiumarra@gmail.com>
2025-05-31 14:06:28 -0700

    match one or more times

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 import sys
 
-# import pyparsing - available if you need it!
-# import lark - available if you need it!
-
 
 def match(pattern, input_line):
     for i in range(len(input_line) + 1):
@@ -13,14 +10,25 @@ def match(pattern, input_line):
     return False
 
 
+# If returns false match() will go through the next iteration of the for loop
+# If returns true then match will return true bc a match was found
 def matchhere(pattern, input_line):
     print("pattern: " + pattern, file=sys.stderr)
     print("input_line: " + input_line, file=sys.stderr)
 
     if not pattern:
         return True
-    # if len(pattern) == 1:
-    #     return pattern in input_line
+    if "+" in pattern:
+        plus_index = pattern.index("+")
+        try:
+            assert plus_index > 0
+        except AssertionError as e:
+            print(f"Need a character preceeding '+': {e}")
+
+        char_pattern = pattern[:plus_index]
+        if char_pattern in input_line:
+            return True
+
     if pattern.startswith("["):
         group, rest, negate_char_group = parse_char_group(pattern)
         if not input_line:
@@ -28,24 +36,21 @@ def matchhere(pattern, input_line):
         if (input_line[0] in group) != negate_char_group:
             return matchhere(rest, input_line[1:])
         return False
-    if pattern.startswith("\d"):
+    if pattern.startswith("\\d"):
         if input_line and input_line[0].isdigit():
             return matchhere(pattern[2:], input_line[1:])
         return False
-    if pattern.startswith("\w"):
+    if pattern.startswith("\\w"):
         if input_line and input_line[0].isalpha():
             return matchhere(pattern[2:], input_line[1:])
         return False
     if pattern == "$" and input_line == "":
-        print(input_line == "", file=sys.stderr)
-        return input_line == ""
+        return True
     if len(input_line) > 0 and pattern[0] == input_line[0]:
-        # print("pattern[1]: " + pattern[1], file=sys.stderr)
-        # print("input_line[1:]: " + input_line[1:], file=sys.stderr)
         return matchhere(pattern[1:], input_line[1:])
     return False
 
-# def matchstar():
+
 def parse_char_group(pattern):
     assert pattern[0] == "["
     negate = False
@@ -71,11 +76,8 @@ def main():
         print("Expected first argument to be '-E'")
         exit(1)
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
 
-    # Uncomment this block to pass the first stage
-    # if match_pattern(input_line, pattern):
     if match(pattern, input_line):
         print("pattern: " + pattern, file=sys.stderr)
         print("exit 0", file=sys.stderr)
